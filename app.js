18edf24394230be6ff88700ce1b534c9ea95ae56
Samuel Idowu <samuelidowu689@gmail.com>
2024-10-16 22:16:16 +0100

    Responsiveness editing

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ app.post('/send-email', (req, res) => {
 
 // app.use(express.static('public'));
   
-app.listen(3000, function() {
-    console.log("Server started on port 3000");
+app.listen(5500, function() {
+    console.log("Server started on port 5500");
 });
 
